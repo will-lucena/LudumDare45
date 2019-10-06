e3e21ccea9ab49a6a1206ca9637a8537b5364336
Will <will.lucen4@gmail.com>
2019-10-06 19:24:25 -0300

    2D pathfinder IA added and first enemy added

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
