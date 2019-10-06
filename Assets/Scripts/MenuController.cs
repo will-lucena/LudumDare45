af9e8692d0d8d140763906cd126fc2222a6774d4
Will <will.lucen4@gmail.com>
2019-10-06 18:36:20 -0300

    Added assets to menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private GameObject mainGO;
+    [SerializeField] private GameObject howGO;
+    [SerializeField] private GameObject aboutGO;
+    [SerializeField] private GameObject settingsGO;
+
+    [SerializeField] private Transform contentGroup;
+    [SerializeField] private GameObject devCardPrefab;
+
+    private GameObject actualMenu;
+    private GameObject lastMenu;
+
+    private void Awake()
+    {
+        SlideAnimationController.notifySlideEnd += startSlideIn;
+    }
+
+    private void Start()
+    {
+        actualMenu = mainGO;
+    }
+
+    public void goToAbout()
+    {
+        lastMenu = actualMenu;
+        actualMenu.GetComponent<Animator>().SetTrigger("slide out");
+        actualMenu = aboutGO;
+    }
+
+    public void goToSettings()
+    {
+        lastMenu = actualMenu;
+        actualMenu.GetComponent<Animator>().SetTrigger("slide out");
+        actualMenu = settingsGO;
+    }
+
+    public void goToMain()
+    {
+        lastMenu = actualMenu;
+        actualMenu.GetComponent<Animator>().SetTrigger("slide out");
+        actualMenu = mainGO;
+    }
+
+    public void goToHow()
+    {
+        lastMenu = actualMenu;
+        actualMenu.GetComponent<Animator>().SetTrigger("slide out");
+        actualMenu = howGO;
+    }
+
+    public void goToGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void quit()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+
+    private void startSlideIn(GameObject obj)
+    {
+        if (obj == lastMenu)
+        {
+            actualMenu.GetComponent<Animator>().SetTrigger("slide in");
+        }        
+    }
+}
